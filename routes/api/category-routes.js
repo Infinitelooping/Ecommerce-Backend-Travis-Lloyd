a828c25bc773b6b6122726edd2c4edc65f424c3f
Travis Lloyd <travis.t.lloyd1996@gmail.com>
2021-12-12 20:50:24 -0700

    added routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,17 +21,47 @@ router.get('/', (req, res) => {
   })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', ({body}, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Post.findOne({
+    where: {
+      id: body.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock'],
+      }
+    ]
+  })
+    .then(dbData => {
+      if (!dbData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+      res.json(dbData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
-router.post('/', (req, res) => {
+router.post('/', ({body}, res) => {
   // create a new category
+  Category.create({
+    title: body.title
+  })
+  .then(dbData => res.json(dbData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  // update a category by its `id` value 
 });
 
 router.delete('/:id', (req, res) => {
