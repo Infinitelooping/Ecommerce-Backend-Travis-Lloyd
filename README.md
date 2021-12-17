28f41ab2e04b32a161f7781d797573ac79945b6b
Travis Lloyd <travis.t.lloyd1996@gmail.com>
2021-12-17 10:58:43 -0700

    finished

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # E-commerce Back End Starter Code
+
+## Live Site
+https://watch.screencastify.com/v/sW3xQAQMFvLBijocuSlG
+
+## Descrption
+Working the backend of an ecommerce site, you are able to add categories and tags, and create prodcuts, update, and delete.
+
+## Made with Love By: Travis Lloyd
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -92,7 +92,11 @@ router.post('/', (req, res) => {
 // update product
 router.put('/:id', (req, res) => {
   // update product data
-  Product.update(req.body, {
+  Product.update(req.body, 
+    {
+      product_name: req.params.product_name
+    },
+    {
     where: {
       id: req.params.id,
     },
